73aa06e52360b07a3e74403ca003481a5ccedaa5
Eugen Kremer <eugen.kremer@siemens.com>
2021-09-19 15:18:45 +0200

    Add timeout parameters

--- src/luxtronik2.ts
+++ src/luxtronik2.ts
@@ -1,13 +1,14 @@
 import WebSocket from "ws";
 import { Access } from "./access";
+import * as events from "events";
 
 type callback = (data:any)=>void;
 
-export class Luxtronik2{
+export class Luxtronik2 extends events.EventEmitter{
     private queue:callback[] = [];
 
     constructor(private readonly ws:WebSocket, public readonly access: Access){
-
+        super();
     }
 
     public disconnect():void{
@@ -29,10 +30,15 @@ export class Luxtronik2{
 
         fn(data);
     }
+    
+    public on(event: 'close', listener: (this: Luxtronik2) => void): this{
+        addEventListener(event, listener);
+        return this;
+    }
 
-    public static async connect(ip:string, password:string): Promise<Luxtronik2>{
+    public static async connect(ip:string, password:string, timeout: number = 100, handshakeTimeout: number = 100): Promise<Luxtronik2>{
         return new Promise((resolve, reject)=>{
-            const ws = new WebSocket(`ws://${ip}:8214`, "Lux_WS");
+            const ws = new WebSocket(`ws://${ip}:8214`, "Lux_WS", { timeout: timeout, handshakeTimeout: handshakeTimeout});
 
             const messageDispatcher = {
                 onmessage : (data: any)=>{
@@ -43,6 +49,8 @@ export class Luxtronik2{
                         }));
 
                         messageDispatcher.onmessage =  lx.onmessage.bind(lx);
+                        
+                        ws.on("close", (ws)=>{lx.emit("close", lx)});
 
                         resolve(lx);
                     }catch(e){
@@ -52,7 +60,6 @@ export class Luxtronik2{
             }
 
             ws.on('error', reject);
-            
             ws.on('message', (data: any)=>{
                messageDispatcher.onmessage(data)     
             });
@@ -60,6 +67,8 @@ export class Luxtronik2{
             ws.on('open', function open() {
                 ws.send("LOGIN;"+password)
               });
+
+            
         })
           
 
