7e1386e4c9a319f2ad54987f0e60f559d0815b53
Eugen Kremer <eugen.kremer@siemens.com>
2022-04-15 12:40:39 +0200

    Add rejection on unexpected response

--- src/luxtronik2.ts
+++ src/luxtronik2.ts
@@ -2,75 +2,88 @@ import WebSocket from "ws";
 import { Access } from "./access";
 import * as events from "events";
 
-type callback = (data:any)=>void;
+type callback = (data: any) => void;
 
-export class Luxtronik2 extends events.EventEmitter{
-    private queue:callback[] = [];
+export class Luxtronik2 extends events.EventEmitter {
+  private queue: callback[] = [];
 
-    constructor(private readonly ws:WebSocket, public readonly access: Access){
-        super();
-    }
+  constructor(private readonly ws: WebSocket, public readonly access: Access) {
+    super();
+  }
 
-    public disconnect():void{
-        this.ws.close();
-    }
+  public disconnect(): void {
+    this.ws.close();
+  }
 
-    private async sendRequest(cmd:string):Promise<any>{
-        return new Promise<any>((resolve, reject)=>{
-            this.queue.push(resolve);
-            this.ws.send(cmd);
-        })
-    }
+  private async sendRequest(cmd: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.queue.push(resolve);
+      this.ws.send(cmd);
+    });
+  }
 
-    private onmessage(data:any){
-        if (this.queue.length <= 0)
-            return;
+  private onmessage(data: any) {
+    if (this.queue.length <= 0) return;
 
-        const fn = this.queue.shift()!;
+    const fn = this.queue.shift()!;
 
-        fn(data);
-    }
-    
-    public on(event: 'close', listener: (this: Luxtronik2) => void): this{
-        addEventListener(event, listener);
-        return this;
-    }
+    fn(data);
+  }
 
-    public static async connect(ip:string, password:string, timeoutMillis: number = 10000, handshakeTimeoutMillis: number = 10000): Promise<Luxtronik2>{
-        return new Promise((resolve, reject)=>{
-            const ws = new WebSocket(`ws://${ip}:8214`, "Lux_WS", { timeout: timeoutMillis, handshakeTimeout: handshakeTimeoutMillis});
+  public on(event: "close", listener: (this: Luxtronik2) => void): this {
+    addEventListener(event, listener);
+    return this;
+  }
 
-            const messageDispatcher = {
-                onmessage : (data: string)=>{
-                    try{
-                        let lx :Luxtronik2;
-                        lx = new Luxtronik2(ws, Access.fromXml(data, (id)=>{
-                            return lx.sendRequest("GET;"+id);
-                        }));
+  public static async connect(
+    ip: string,
+    password: string,
+    timeoutMillis: number = 10000,
+    handshakeTimeoutMillis: number = 10000
+  ): Promise<Luxtronik2> {
+    return new Promise((resolve, reject) => {
+      try {
+        const ws = new WebSocket(`ws://${ip}:8214`, "Lux_WS", {
+          timeout: timeoutMillis,
+          handshakeTimeout: handshakeTimeoutMillis,
+        });
 
-                        messageDispatcher.onmessage =  lx.onmessage.bind(lx);
-                        
-                        ws.on("close", (ws)=>{lx.emit("close", lx)});
+        const messageDispatcher = {
+          onmessage: (data: string) => {
+            try {
+              let lx: Luxtronik2;
+              lx = new Luxtronik2(
+                ws,
+                Access.fromXml(data, (id) => {
+                  return lx.sendRequest("GET;" + id);
+                })
+              );
 
-                        resolve(lx);
-                    }catch(e){
-                        reject(e);
-                    }
-                }
-            }
+              messageDispatcher.onmessage = lx.onmessage.bind(lx);
 
-            ws.on('error', reject);
-            ws.on('message', (data: WebSocket.Data)=>{
-               messageDispatcher.onmessage(data.toString())     
-            });
-
-            ws.on('open', function open() {
-                ws.send("LOGIN;"+password)
+              ws.on("close", (ws) => {
+                lx.emit("close", lx);
               });
 
-            
-        })
-          
+              resolve(lx);
+            } catch (e) {
+              reject(e);
+            }
+          },
+        };
 
-    }
+        ws.on("unexpected-response", reject);
+        ws.on("error", reject);
+        ws.on("message", (data: WebSocket.Data) => {
+          messageDispatcher.onmessage(data.toString());
+        });
+
+        ws.on("open", function open() {
+          ws.send("LOGIN;" + password);
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
 }
